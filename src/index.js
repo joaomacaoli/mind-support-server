0ad69af003c1c69792c4a2e60ef46cda7c78559f
João Macaoli <devjoaom@gmail.com>
2025-02-16 16:38:27 -0300

    feat: create index.js file

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+// import cors from "cors";
+import routes from "./routes/index.js";
+import "dotenv/config.js";
+
+const app = express();
+
+// const frontendPort = process.env.FRONT_PORT;
+// app.use(cors({ origin: `http://localhost:${frontendPort}` }));
+
+const backendPort = process.env.SERVER_PORT || 3000;
+const message = console.log(
+  `Servidor rodando em http://localhost:${backendPort}`
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(backendPort, () => {
+  message;
+});
